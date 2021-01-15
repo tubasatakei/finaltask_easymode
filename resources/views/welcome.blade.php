100cfbf389a469eee44f0b96aa44749d35b7c94d
tubasa.takei <fhleben.dntsygdby@gmail.com>
2021-01-15 17:18:02 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+            <h1>Finaltask</h1>
         </div>
     </div>
 @endsection
